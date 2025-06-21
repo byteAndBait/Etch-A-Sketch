cbab608826daa315ab5144b2331ab72239f8c863
byteAndBait <192332038+byteAndBait@users.noreply.github.com>
2025-06-21 10:58:28 +0300

    fix: fix the darkening effect
     the box now turn black after ten interactions

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let container = document.querySelector(".container");
 const button = document.getElementById("gridPromptOpen");
-let input = 16;
+let input = 2;
+
 makeGrid();
 button.addEventListener("click", () => {
   input = prompt("Enter the number of grids");
@@ -9,38 +10,43 @@ button.addEventListener("click", () => {
     input = 16;
     return;
   }
-  document.querySelectorAll(".box").forEach(e => {
+  document.querySelectorAll(".box").forEach((e) => {
     e.remove();
   });
   makeGrid();
 });
+
 function makeGrid() {
   for (let i = 0; i < input * input; i++) {
     let box = document.createElement("div");
     box.classList.add("box");
-    box.style.cssText = `flex: 1 ${(input / (input * input)) * 100}%`;
+    box.style.cssText = `flex: 1 ${(1 / input) * 100}%`;
     container.appendChild(box);
   }
 }
+
 function getRandom(max) {
   return parseInt(Math.random() * max);
 }
-
-container.addEventListener("mouseover", e => {
+let tenPecentMinus
+container.addEventListener("mouseover", (e) => {
   if (e.target.classList.contains("box")) {
     if (e.target.style.background) {
       e.target.setAttribute(
         "data-r",
-        e.target.getAttribute("data-r") - 0.1 * e.target.getAttribute("data-r")
+        e.target.getAttribute("data-r") -tenPecentMinus
       );
+
       e.target.setAttribute(
         "data-g",
-        e.target.getAttribute("data-g") - 0.1 * e.target.getAttribute("data-g")
+        e.target.getAttribute("data-g") - tenPecentMinus
       );
+
       e.target.setAttribute(
         "data-b",
-        e.target.getAttribute("data-b") - 0.1 * e.target.getAttribute("data-b")
+        e.target.getAttribute("data-b") -tenPecentMinus
       );
+
       e.target.style.background = `rgb(${e.target.getAttribute(
         "data-r"
       )},${e.target.getAttribute("data-g")},${e.target.getAttribute(
@@ -50,6 +56,11 @@ container.addEventListener("mouseover", e => {
       e.target.setAttribute("data-r", getRandom(255));
       e.target.setAttribute("data-g", getRandom(255));
       e.target.setAttribute("data-b", getRandom(255));
+      
+       tenOfR = 0.1 * e.target.getAttribute("data-r");
+       tenOfG = 0.1 * e.target.getAttribute("data-g");
+       tenOfB = 0.1 * e.target.getAttribute("data-b");
+       tenPecentMinus = 0.1 * Math.max(e.target.getAttribute("data-r"),e.target.getAttribute("data-g"),e.target.getAttribute("data-b"))
       e.target.style.background = `rgb(${e.target.getAttribute(
         "data-r"
       )},${e.target.getAttribute("data-g")},${e.target.getAttribute(
