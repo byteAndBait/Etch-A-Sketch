0c49e2b4fa04f99c33325567d36a6bee8e5bc49b
byteAndBait <abdulrahman.elkassas@protonmail.com>
2026-07-16 16:03:35 +0300

    fix: Correct darkening behavior and few severeal enhancements

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 const container = document.querySelector(".container");
-const inputElement = document.getElementById("gridNumber")
-let input = inputElement.value;
+const inputElement = document.getElementById("numberOfGridsInput");
 
 makeGrid();
 inputElement.addEventListener("input", () => {
-  input = inputElement.value;
-  container.textContent = "";
   makeGrid();
 });
 
 function makeGrid() {
-  for (let i = 0; i < input ** 2; i++) {
-    let box = document.createElement("div");
+  container.textContent = "";
+  for (let i = 0; i < inputElement.value ** 2; i++) {
+    const box = document.createElement("div");
     box.classList.add("box");
-    box.style.flex = `1 ${(1 / input) * 100}%`;
+    box.style.flexBasis = `${(1 / inputElement.value) * 100}%`;
     container.appendChild(box);
   }
 }
@@ -21,26 +19,33 @@ function makeGrid() {
 function getRandom(max) {
   return parseInt(Math.random() * max);
 }
-let rColor,gColor,bColor, tenPercentMinus;
+function setRGB(element) {
+  element.style.setProperty("--r", getRandom(255));
+  element.style.setProperty("--g", getRandom(255));
+  element.style.setProperty("--b", getRandom(255));
+}
 container.addEventListener("mouseover", (e) => {
-  if (e.target.classList.contains("box")) {
-    if (getComputedStyle(e.target).getPropertyValue("--white-back") == 1) {
-      e.target.style.setProperty("--r", getRandom(255));
-      e.target.style.setProperty("--g", getRandom(255));
-      e.target.style.setProperty("--b", getRandom(255));
-      rColor = +getComputedStyle(e.target).getPropertyValue("--r");
-      gColor = +getComputedStyle(e.target).getPropertyValue("--g");
-      bColor = +getComputedStyle(e.target).getPropertyValue("--b");
-      tenPercentMinus = Math.max(rColor,gColor,bColor) * 0.1
-      e.target.style.setProperty("--white-back", 0);
-    } else {
-       rColor = +getComputedStyle(e.target).getPropertyValue("--r");
-       gColor = +getComputedStyle(e.target).getPropertyValue("--g");
-       bColor = +getComputedStyle(e.target).getPropertyValue("--b");
-      console.log(tenPercentMinus)
-      e.target.style.setProperty("--r", rColor -tenPercentMinus);
-      e.target.style.setProperty("--g", gColor - tenPercentMinus);
-      e.target.style.setProperty("--b", bColor - tenPercentMinus);
+  const element = e.target;
+  if (element.classList.contains("box")) {
+    const isWhite = getComputedStyle(element).getPropertyValue("--white-back")
+    if (isWhite == 1) {
+      setRGB(element);
+      element.style.setProperty("--white-back", 0);
     }
   }
 });
+
+container.addEventListener("click", (e) => {
+  const element = e.target;
+  if (element.classList.contains("box")) {
+    const isWhite = getComputedStyle(element).getPropertyValue("--white-back")
+
+    if (isWhite == 0) // if it is has a value
+    {
+      let currentBrightness = +getComputedStyle(element).getPropertyValue("--brightness") * 100;
+      if (currentBrightness > 0) {
+        element.style.setProperty("--brightness", (currentBrightness - 10) / 100);
+      }
+    }
+  }
+})
